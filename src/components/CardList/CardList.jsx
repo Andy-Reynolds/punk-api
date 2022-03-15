4d591ff150a0b71f6ef137c2f3243679ddc1e6ff
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-03-15 13:19:46 +0000

    All cards showing by default. Can search for case sensitive beers

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -3,7 +3,7 @@ import Card from '../Card/Card'
 import "./CardList.scss"
 
 const CardList = ({beers, searchedBeer}) => {
-  return beers.filter(beer => beer.name === searchedBeer).map((beer) => {
+  return beers.filter(beer => beer.name.includes(searchedBeer)).map((beer) => {
     return (
       <Card key={beer.id} beer={beer} />
     )
