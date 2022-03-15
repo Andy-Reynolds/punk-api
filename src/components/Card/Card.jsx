06838fe7dd4754b5549d7ee4d095d23aa52a0abb
Andy-Reynolds <andy.reynolds@hotmail.co.uk>
2022-03-15 12:26:58 +0000

    Added some basic responsivity

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,16 +8,18 @@ const Card = ({beer}) => {
 
   return (
     <div className='beer-card'>
-      <h3 className='beer-card__name'>{beer.name}</h3>
-      <div className="beer-card__image-container">
-        <img className='beer-card__image' src={beer.image_url} alt="" />
-      </div>
-      <div className="beer-card__info">
-        <p className='beer-card__info--abv'><span className='prefix'>ABV:</span> {beer.abv}%</p>
-        <p className='beer-card__info--first-brewed'><span className='prefix'>First brewed:</span> {beer.first_brewed}</p>
-        <p className='beer-card__info--ph'><span className='prefix'>ph:</span> {beer.ph}</p>
-        <p className='beer-card__info--food-pairing'><span className='prefix'>Food Pairing:</span> {beer.food_pairing.join(", ")}</p>
-        <p className='beer-card__info--description'><span className='prefix'>Description:</span> {beer.description}</p>
+      <div className="beer-card__content">
+        <h3 className='beer-card__name'>{beer.name}</h3>
+        <div className="beer-card__image-container">
+          <img className='beer-card__image' src={beer.image_url} alt="" />
+        </div>
+        <div className="beer-card__info">
+          <p className='beer-card__info--abv'><span className='prefix'>ABV:</span> {beer.abv}%</p>
+          <p className='beer-card__info--first-brewed'><span className='prefix'>First brewed:</span> {beer.first_brewed}</p>
+          <p className='beer-card__info--ph'><span className='prefix'>ph:</span> {beer.ph}</p>
+          <p className='beer-card__info--food-pairing'><span className='prefix'>Food Pairing:</span> {beer.food_pairing.join(", ")}</p>
+          <p className='beer-card__info--description'><span className='prefix'>Description:</span> {beer.description}</p>
+        </div>
       </div>      
     </div>
   )
